d7f210cce88c195c8a22f3e131ff416f38bca609
Meqdad Ali <meqdadali@Meqdads-MacBook-Pro.local>
2021-04-08 09:49:46 -0400

    Jquerry HW

---
+++ random-imager/app.js
@@ -0,0 +1,36 @@
+const cards = [
+    'https://www.trustedtarot.com/img/cards/the-fool.png',
+    'https://www.trustedtarot.com/img/cards/the-magician.png',
+    'https://www.trustedtarot.com/img/cards/the-high-priestess.png',
+    'https://www.trustedtarot.com/img/cards/the-empress.png',
+    'https://www.trustedtarot.com/img/cards/the-emperor.png',
+    'https://www.trustedtarot.com/img/cards/the-heirophant.png',
+    'https://www.trustedtarot.com/img/cards/the-chariot.png',
+    'https://www.trustedtarot.com/img/cards/strength.png',
+    'https://www.trustedtarot.com/img/cards/the-hermit.png',
+    'https://www.trustedtarot.com/img/cards/wheel-of-fortune.png',
+    'https://www.trustedtarot.com/img/cards/justice.png',
+    'https://www.trustedtarot.com/img/cards/the-hanged-man.png',
+    'https://www.trustedtarot.com/img/cards/death.png',
+    'https://www.trustedtarot.com/img/cards/temperance.png',
+    'https://www.trustedtarot.com/img/cards/the-tower.png',
+    'https://www.trustedtarot.com/img/cards/the-moon.png'
+]
+const $body = $("body");
+const $div = $("<div>").text("Add random card")
+$body.append($div);
+console.log($div)
+
+const $cards = $("<cards>")
+$div.append($cards);
+
+
+const $makeCard = () => {
+    Math.floor((Math.random() * cards.length));
+    console.log()
+}
+
+
+$div.on("click", (event) => {
+    $makeCard()
+})
